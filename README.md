d9da15f9ce3caaa3aff429b68a9173fbe355c6a1
Rickiarty <fatalframe0719@gmail.com>
2024-07-21 22:21:55 +0800

    The purpose is to show you what HTTP protocol is.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # stiff HTTP server from TCP scratch
 <p>It's a stiff HTTP server-side program from TCP scratch. <br>
-Whatever HTTP request you send to it, it always gives you an identical HTTP response.</p>
+Whatever HTTP request you send to it, it always gives you an identical HTTP response. <br>
+The purpose is to show you what HTTP protocol is.</p>
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use std::thread;
 use chrono::Utc;
 
 fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0; 4096*3];
+    let mut buffer = [0; 4096*10*3];
     let bytes_read = match stream.read(&mut buffer) {
         Ok(bytes) => bytes,
         Err(e) => {
-            eprintln!("讀取 socket 失敗：{}", e);
+            eprintln!("\n讀取 socket 失敗：{}\n", e);
             return;
         }
     };
@@ -17,26 +17,26 @@ fn handle_client(mut stream: TcpStream) {
     let request = match std::str::from_utf8(&buffer[0..bytes_read]) {
         Ok(v) => v.to_string(),
         Err(e) => {
-            println!("無效的 UTF-8 序列：{}", e);
+            println!("\n無效的 UTF-8 序列：{}\n", e);
             return;
         },
     };
-    println!("{}\n\n======\n", request);
+    println!("\nHTTP request：\n\n{}\n\n[C]======>[S]\n", request);
 
-    let response_body = "<!DOCTYPE html>\n<html>\n<head>\n  <title>An Example Page</title>\n</head>\n<body>\n  Hello World.\n</body>\n</html>";
+    let response_body = "<!DOCTYPE html>\n<html>\n<head>\n  <title>An Example Page</title>\n</head>\n<body>\n  Hello, world!\n</body>\n</html>";
     let current_time = Utc::now().format("%a, %d %b %Y %T GMT").to_string();
-    let response_header = format!("HTTP/1.1 200 OK\nDate: {}\nServer: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)\nLast-Modified: Wed, 08 Jan 2003 23:11:55 GMT\nETag: \"3f80f-1b6-3e1cb03b\"\nContent-Type: text/html; charset=UTF-8\nContent-Length: {}\nAccept-Ranges: bytes\nConnection: close\n", current_time, response_body.chars().count());
-    let response = format!("{}\r\n{}", response_header, response_body);
+    let response_header = format!("HTTP/1.1 200 OK\nDate: {}\nServer: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)\nLast-Modified: Wed, 08 Jan 2003 23:11:55 GMT\nETag: \"3f80f-1b6-3e1cb03b\"\nContent-Type: text/html; charset=UTF-8\nContent-Length: {}\nAccept-Ranges: bytes\nConnection: close", current_time, response_body.chars().count());
+    let response = format!("{}\n\r\n{}", response_header, response_body);
     let res_bytes = response.as_bytes();
 
-    println!("字串：{}", response);
-    println!("編碼後的 byte array：{:?}", res_bytes);
+    println!("\nHTTP response：\n\n{}\n\n[C]<=======[S]\n", response);
+    println!("\n編碼後的 byte array：\n{:?}\n\n*========*\n", res_bytes);
 
     if let Err(e) = stream.write_all(&res_bytes) {
-        eprintln!("寫入 socket 失敗：{}", e);
+        eprintln!("\n寫入 socket 失敗：{}\n", e);
     }
     if let Err(e) = stream.flush() {
-        eprintln!("刷新 socket 失敗：{}", e);
+        eprintln!("\n刷新 socket 失敗：{}\n", e);
     }
 }
 
@@ -47,7 +47,7 @@ fn main() {
             Ok(stream) => {
                 thread::spawn(|| { handle_client(stream); });
             }
-            Err(e) => { eprintln!("無法連接：{}", e); }
+            Err(e) => { eprintln!("\n無法連接：{}\n", e); }
         }
     }
 }
